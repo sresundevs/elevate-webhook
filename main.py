63b188bb9eb7078ea8aada0b312532535c972616
David Alejandro Morales Delgado <alejodavidibague.98@gmail.com>
2023-12-13 11:36:22 -0500

    config PROD

--- main.py
+++ main.py
@@ -13,19 +13,19 @@ from routes.users import users_bp
 from routes.webhooks import webhooks_bp
 
 config = {
-    **dotenv_values(".env"),    # load development variables
-    **os.environ,               # override loaded values with environment variables
+    **dotenv_values(".env"),  # load development variables
+    **os.environ,  # override loaded values with environment variables
 }
 
 db = connect_db(True)
 
-#Flask app to listen Webhook from Hotmart
+# Flask app to listen Webhook from Hotmart
 app = Flask(__name__)
 
-#Enable CORS
+# Enable CORS
 CORS(app)
 
-#Register blueprints
+# Register blueprints
 app.register_blueprint(webhooks_bp)
 app.register_blueprint(customer_bp)
 app.register_blueprint(purchases_bp)
@@ -33,12 +33,12 @@ app.register_blueprint(escalations_bp)
 app.register_blueprint(users_bp)
 app.register_blueprint(auth_bp)
 
-@app.route('/health', methods=['GET'])
+
+@app.route("/health", methods=["GET"])
 def handle_check():
-    return 'OK'
-
-if __name__ == '__main__':
-    app.run(host=config['HOST'],port=config['PORT'])  #To run in local
-    #app.run(port=os.getenv("PORT", default=5000)) #To run in PROD
+    return "OK"
 
 
+if __name__ == "__main__":
+    # app.run(host=config['HOST'],port=config['PORT'])  #To run in local
+    app.run(port=os.getenv("PORT", default=5000))  # To run in PROD
